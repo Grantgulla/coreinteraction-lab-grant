912400697fa0da2cd1955f7e55113da178797059
Grant Gulla <grantgulla@live.com>
2016-03-30 19:26:37 -0400

    Week 7

---
+++ Week 7/week7.js
@@ -0,0 +1,70 @@
+
+//1
+var a = 'hello world';
+
+console.log (a);
+
+
+
+
+
+//2
+var a = 'hello';
+var b = "world";
+
+console.log(a + b); 
+
+
+
+
+//3
+var a = 'hello';
+var b = 'goodbye'
+
+if (a = 'hello') {
+	console.log ('hi')
+}
+
+if (b = 'goodbye') {
+	console.log ('bye')
+}
+
+
+
+
+//4
+function isGreaterThanTen (number){
+	if(number>10)
+		return true;
+} else {
+	return false;
+}
+}
+console.log (isGreaterThanTen(2));
+
+//5
+function printOneThruTen(){
+	for (var i = i; i<11; ++i){
+		console.log(i);
+	}
+}
+
+
+//6
+function evaluateOneThruTwenty(){
+	for (var i=i; i <21; ++i){
+		if (i<10){
+			console.log('small');
+		}else {
+			console.log('large');
+					}
+		}
+	}
+}
+
+
+
+
+
+
+
